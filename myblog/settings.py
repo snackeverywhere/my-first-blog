96410ee25821a8fb044a6824ebbda99bca382205
MiaTran <ttdiemquyen@live.com>
2018-04-05 10:35:59 +1000

    modified setting static

--- myblog/settings.py
+++ myblog/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "myblog/static"),
 )
 
 STATIC_URL = '/static/'
